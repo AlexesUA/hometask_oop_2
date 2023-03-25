30a282066525b71b9ca6dc0eefc9a71f8ea77105
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-03-25 18:06:58 +0200

    fix some bags

--- src/task_5/Main.java
+++ src/task_5/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Random rmdNumber = new Random();
         for(int i=0; i<5; i++){
-            double radius = rmdNumber.nextDouble();
+            double radius = i+1;//rmdNumber.nextDouble(1,10);
             System.out.printf("Площа кола з радіусом %1$.2f дорівнює %2$.2f %n", radius, MyArea.AreaOfCircle(radius));
 
         }
