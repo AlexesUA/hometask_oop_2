967e6288f16845c1a691bf3372187a4c246d456a
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-03-25 17:55:37 +0200

    finish class MyArea & create main class

--- src/task_5/MyArea.java
+++ src/task_5/MyArea.java
@@ -1,4 +1,10 @@
 package task_5;
 
 public class MyArea {
+    static final double PI = 3.14;
+
+    static public double AreaOfCircle(double radius){
+        return PI*Math.pow(radius, 2);
+    }
+
 }
